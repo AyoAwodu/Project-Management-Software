afc8b05855d15d8bb91ae8a9a2e1b5d34df70fdc
Suzanna Carter <suzannacarter@gmail.com>
2019-07-14 20:55:02 -0400

    Add Data Storage and Retreival

---
+++ ProjectManagementSoftware/Data.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+
+namespace ProjectManagementSoftware
+{
+    public partial class Data
+    {
+        [JsonProperty("Projects")]
+        public Project[] Projects { get; set; }
+
+    }
+
+    public class Project {
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("Owner")]
+        public string Owner { get; set; }
+
+        [JsonProperty("Risks")]
+        public Risk[] Risks { get; set; }
+
+        [JsonProperty("FunctionalRequirements")]
+        public string[] FunctionalRequirements { get; set; }
+
+        [JsonProperty("NonFunctionalRequirements")]
+        public string[] NonFunctionalRequirements { get; set; }
+
+        [JsonProperty("TimeSheet")]
+        public TimeSheet TimeSheet { get; set; }
+
+    }
+
+    public class Risk {
+
+        [JsonProperty("RiskFactor")]
+        public string RiskFactor { get; set; }
+
+        [JsonProperty("Status")]
+        public string Status { get; set; }
+
+    }
+
+    public class TimeSheet {
+
+        [JsonProperty("RequirementsAnalysis")]
+        public double RequirementsAnalysis { get; set; }
+
+        [JsonProperty("Designing")]
+        public double Designing { get; set; }
+
+        [JsonProperty("Coding")]
+        public double Coding { get; set; }
+
+        [JsonProperty("Testing")]
+        public double Testing { get; set; }
+
+        [JsonProperty("ProjectManagement")]
+        public double ProjectManagement { get; set; }
+
+    }
+
+}
--- ProjectManagementSoftware/MainWindow.xaml.cs
+++ ProjectManagementSoftware/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace ProjectManagementSoftware
 {
@@ -20,9 +22,44 @@ namespace ProjectManagementSoftware
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Data DataContent { get; set; }
+
+        // Any CPU
+        private string jsonPath = "../../Data.json";
+        //private string jsonPath = "Data.json"; // 64bit
+
+        private string Contents { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Contents = File.ReadAllText(jsonPath);
+            DataContent = JsonConvert.DeserializeObject<Data>(Contents);
+            // DataContent can now be used to read and change fields in Data.json
+            // All fields should be marked as required before altering DataContent
+            // Some sample data of what one project may look like is included in Data.json
+
+            // Here is how you would add a new project to DataContent
+            Project newProject = new Project
+            {
+                Name = "",
+                Description = ""
+                // more
+            };
+
+            DataContent.Projects.Append(newProject);
+            // Then call UpdateJsonFile()
+            UpdateJsonFile();
+
+        }
+
+        // Call this function after changing any attribute of DataContent
+        private void UpdateJsonFile()
+        {
+            string updatedFile = JsonConvert.SerializeObject(DataContent);
+            File.WriteAllText(jsonPath, updatedFile);
         }
     }
 }
